54ed872f449644629c09dfdfe0704da6d9b6beef
CallMe-MrPotato <virajkadbe123@gmail.com>
2024-12-30 13:11:28 +0530

    Hibernate_Aadhar

--- src/com/jbk/dec30/Main.java
+++ src/com/jbk/dec30/Main.java
@@ -5,19 +5,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import com.arjuna.ats.arjuna.recovery.Service;
-
 public class Main {
+
 	public static void main(String[] args) {
 		Configuration cfg = new Configuration();
 		cfg.configure("hibernate.cfg.xml");
-		cfg.addAnnotatedClass(Employee.class);
+		cfg.addAnnotatedClass(Aadhar.class);
 		SessionFactory sf = cfg.buildSessionFactory();
 		Session ss = sf.openSession();
 		Transaction ts = ss.beginTransaction();
 		
-		Employee e = new Employee();
-		System.out.println("Application is running...");
+		Aadhar a = new Aadhar();
+		System.out.println("Application is Running....");
 	}
-
 }
